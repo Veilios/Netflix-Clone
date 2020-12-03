fd5ab5091b3a0fee2d9c97ce6d7f0313bff36d6e
Alan Mir <alanpaulphotos@gmail.com>
2020-12-02 20:34:13 -0500

    Fixed footer grid bug

--- netflix/src/components/footer/styles/footer.js
+++ netflix/src/components/footer/styles/footer.js
@@ -22,11 +22,11 @@ export const Column = styled.div`
 
 export const Row = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(210px, lfr));
+    grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
     grid-gap: 15px;
 
     @media (max-width: 1000px) {
-        grid-template-columns: repeat(auto-fill, minmax(150px, lfr));
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
     }
 
 `;
