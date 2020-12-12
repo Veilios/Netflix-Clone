1b1794d16cb8f9d3d8521e85504b47aa6f777e76
Alan Mir <alanpaulphotos@gmail.com>
2020-12-11 20:07:12 -0500

    Uninstalled react router and reinstalled in the correct folder

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
+import * ROUTES from './constants/routes';
 
 export default function App() {
   return (
