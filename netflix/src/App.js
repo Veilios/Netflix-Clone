9729e47e3f75c2154059c75f760aafd300de0e58
Alan Mir <alanpaulphotos@gmail.com>
2020-12-11 20:44:24 -0500

    Added the page routes to the app.js

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -9,6 +9,15 @@ export default function App() {
       <Route exact path={ROUTES.HOME}>
         <Home />
       </Route>
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
     </Router>
   )
 };
